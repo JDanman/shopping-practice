dc1f6412b6d41b9a4b9675ba789d765060ee595a
J.Daniel Pérez García <perezg.jdaniel@outlook.com>
2019-02-13 16:50:58 -0600

    Adding Style & Design

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 var app = new Vue({
     el: '#app',
     data: {
-        product: 'Socks'
+        product: 'Socks',
+        description: 'A pair of warm, fuzzy socks.',
+        image: './assets/vmSocks-green.jpg',
+        img_src: 'https://www.dropbox.com/s/9zccs3f0pimj0wj/vmSocks-green-onWhite.jpg?dl=0'
     }
 })
 
