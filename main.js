d847ceb1e11ea24f45f0a4a5f7272b2f9b9ea75e
J.Daniel Pérez García <perezg.jdaniel@outlook.com>
2019-02-13 16:59:54 -0600

    Adding boolean conditionals

--- main.js
+++ main.js
@@ -4,7 +4,8 @@ var app = new Vue({
         product: 'Socks',
         description: 'A pair of warm, fuzzy socks.',
         image: './assets/vmSocks-green.jpg',
-        img_src: 'https://www.dropbox.com/s/9zccs3f0pimj0wj/vmSocks-green-onWhite.jpg?dl=0'
+        img_src: 'https://www.dropbox.com/s/9zccs3f0pimj0wj/vmSocks-green-onWhite.jpg?dl=0',
+        stock: true
     }
 })
 
