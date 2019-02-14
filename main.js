ff053f2b2bf886b6f9dc2ad6733a229df5d6d7c4
J.Daniel Pérez García <perezg.jdaniel@outlook.com>
2019-02-14 11:45:02 -0600

    Adding Lists & Arrays of Values

--- main.js
+++ main.js
@@ -6,6 +6,31 @@ var app = new Vue({
         image: './assets/vmSocks-green.jpg',
         img_src: 'https://www.dropbox.com/s/9zccs3f0pimj0wj/vmSocks-green-onWhite.jpg?dl=0',
         inventory: 5,
-        onSale: true
+        onSale: true,
+        details: ["80% Cotton", "20% Polyester", "Gender-Neutral"],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: "green"
+            },
+            {
+                variantId: 2235,
+                variantColor: "blue"
+            }
+        ],
+        sizes: [
+            {
+                sizeId: 001,
+                sizeName: "Small"
+            },
+            {
+                sizeId: 002,
+                sizeName: "Medium"
+            },
+            {
+                sizeId: 003,
+                sizeName: "Large"
+            }
+        ]
     }
 })
